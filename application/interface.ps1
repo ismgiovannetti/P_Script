ee867696d8d25b080a6963dc1bb5aa6a8348d778
ismgiovannetti <59878033+ismgiovannetti@users.noreply.github.com>
2020-06-03 23:47:12 +0200

    Final Uptade - Ismael Tiago
    Mise en forme de l'interface
    ajout des dernière fonctionnalités

--- application/interface.ps1
+++ application/interface.ps1
@@ -1,7 +1,61 @@
 ﻿#Auteur: Ismael Giovannetti
 #Date: 27.03.2020
 #Modif:
+#Fonctions Boutons
+function ShowUser
+{
+$InfoUser = Get-WmiObject -Class Win32_UserAccount -Filter  "LocalAccount='True'" | Select PSComputername, Name, Status, Disabled, AccountType, Lockout, PasswordRequired, PasswordChangeable | Out-GridView
+#------------------------------
+}
+function ShowGPU
+{
+$InfoGPU = Get-WmiObject -Class win32_VideoController | Select Name, CurrentHorizontalResolution, CurrentVerticalResolution, Status | Out-GridView
+}
+function ShowDrive
+{
+ $Status_all_disk = Get-WmiObject -Class Win32_LogicalDisk -Filter "DriveType=3"| Select SystemName,DeviceID,VolumeName,@{Name="Taille(Go)";Expression={"{0:N1}"-f($_.size/1GB)}},@{Name="Espace Restant(Go)";Expression={"{0:N1}"-f($_.freespace/1GB)}} | Out-GridView
+}
+function ShowRam
+{
+$InfoGPU = Get-WmiObject -Class Win32_physicalmemory | Select Name, Capacity, ConfiguredClockSpeed | Out-GridView
+}
 
+function ShowCPU
+{
+$InfoGPU = Get-WmiObject -Class Win32_processor | Select Name, LoadPercentage, CurrentClockSpeed,Availability | Out-GridView
+}
+function ShowStart
+{
+$InfoGPU = Get-CimInstance -ClassName Win32_StartupCommand | Select-Object -Property Name | Out-GridView
+}
+#---------------------
+
+#evenement boutons 
+$btnUser_OnClick= 
+{
+    ShowUser
+}
+$btnGPU_OnClick= 
+{
+    ShowGPU   
+}
+$btnDrive_OnClick= 
+{
+    ShowDrive   
+}
+$btnRam_OnClick= 
+{
+    ShowRam   
+}
+$btnCPU_OnClick= 
+{
+    ShowCPU   
+}
+$btnStart_OnClick= 
+{
+    ShowStart  
+}
+#--------------------
 function GenerateForm
 {
 Add-Type -AssemblyName System.Windows.Forms
@@ -12,58 +66,87 @@ $frmMain = New-Object System.Windows.Forms.Form
 
 ##Création d'une variable "Size"
 $FormSize = New-Object System.Drawing.Size
-$FormSize.Height = 500
-$FormSize.Width = 800
+$FormSize.Height = 350
+$FormSize.Width = 240
 $frmMain.ClientSize = $FormSize
 #------------------------------
 
 ##Propriété de la Form
 $frmMain.FormBorderStyle = 'FixedSingle'
 #Permet oui ou non de mettre en fullscreen
-$frmMain.MaximizeBox = $true
+$frmMain.MaximizeBox = $false
 #$frmMain.DataBindings.DefaultDataSourceUpdateMode = 0
 $frmMain.Name = "frmMain"
 $frmMain.Text = [system.environment]::MachineName
 #------------------------------
 
-#User
-#$i = 0
-$lblUser = New-Object System.Windows.Forms.Label
-$lblUser.Location = New-Object System.Drawing.Point(10,120)
-$lblUser.AutoSize = $true
-$InfoUser = Get-WmiObject -Class Win32_UserAccount -Filter  "LocalAccount='True'" | Select PSComputername, Name, Status, Disabled, AccountType, Lockout, PasswordRequired, PasswordChangeable | Out-GridView
-$lblUser.Text = "User: " + $InfoUser.Name + " "
-$frmMain.Controls.Add($lblUser)
-#------------------------------
-
 #Nom de la machine
 $lblMachineName = New-Object System.Windows.Forms.Label
-$lblMachineName.Location = New-Object System.Drawing.Point(10,20)
+$lblMachineName.Location = New-Object System.Drawing.Point(65,30)
 $lblMachineName.AutoSize = $true
-$lblMachineName.Text = "Nom: "+[system.environment]::MachineName
+$lblMachineName.Text = [system.environment]::MachineName
 $frmMain.Controls.Add($lblMachineName)
 #------------------------------
 
-#Ram
-$lblRam = New-Object System.Windows.Forms.Label
-$lblRam.Location = New-Object System.Drawing.Point(10,40)
-$lblRam.AutoSize = $true
-$Taille_RAM_MAX=[STRING]((Get-WmiObject -Class Win32_ComputerSystem ).TotalPhysicalMemory/1GB)
-$lblRam.Text = "Ram: "+ $Taille_RAM_MAX
-$frmMain.Controls.Add($lblRam)
+
+#Bouton User
+$btnUser = New-Object System.Windows.Forms.Button
+$btnUser.Location = New-Object System.Drawing.Point(80,70)
+$btnUser.AutoSize = $true
+$btnUser.Text = "User"
+$btnUser.add_Click($btnUser_OnClick)
+$frmMain.Controls.Add($btnUser)
+
 #------------------------------
 
-#Carte Graphique
-$lblVideoController = New-Object System.Windows.Forms.Label
-$lblVideoController.Location = New-Object System.Drawing.Point(10,60)
-$lblVideoController.AutoSize = $true
-$VideoControllerName = wmic path win32_VideoController get Name
-$VideoControllerResolution = wmic path win32_VideoController get CurrentHorizontalResolution
-$VideoControllerResolution += wmic path win32_VideoController get CurrentVerticalResolution
-$lblVideoController.Text = "Carte graphique: `n"+ $VideoControllerName + "`n" + $VideoControllerResolution
-$frmMain.Controls.Add($lblVideoController)
+#Bouton GPU
+$btnGPU = New-Object System.Windows.Forms.Button
+$btnGPU.Location = New-Object System.Drawing.Point(80,110)
+$btnGPU.AutoSize = $true
+$btnGPU.Text = "GPU"
+$btnGPU.add_Click($btnGPU_OnClick)
+$frmMain.Controls.Add($btnGPU)
+
 #------------------------------
 
+#Bouton Disque
+$btnDrive = New-Object System.Windows.Forms.Button
+$btnDrive.Location = New-Object System.Drawing.Point(80,150)
+$btnDrive.AutoSize = $true
+$btnDrive.Text = "Drive"
+$btnDrive.add_Click($btnDrive_OnClick)
+$frmMain.Controls.Add($btnDrive)
+
+#------------------------------
+
+#Bouton Ram
+$btnRam = New-Object System.Windows.Forms.Button
+$btnRam.Location = New-Object System.Drawing.Point(80,190)
+$btnRam.AutoSize = $true
+$btnRam.Text = "Ram"
+$btnRam.add_Click($btnRam_OnClick)
+$frmMain.Controls.Add($btnRam)
+
+#------------------------------
+
+
+#Bouton CPU
+$btnCPU = New-Object System.Windows.Forms.Button
+$btnCPU.Location = New-Object System.Drawing.Point(80,230)
+$btnCPU.AutoSize = $true
+$btnCPU.Text = "CPU"
+$btnCPU.add_Click($btnCPU_OnClick)
+$frmMain.Controls.Add($btnCPU)
+#------------------------------
+
+#Bouton Start
+$btnStart = New-Object System.Windows.Forms.Button
+$btnStart.Location = New-Object System.Drawing.Point(80,270)
+$btnStart.AutoSize = $true
+$btnStart.Text = "Start"
+$btnStart.add_Click($btnStart_OnClick)
+$frmMain.Controls.Add($btnStart)
+#------------------------------
 
 #Génération de la Form
 $frmMain.add_Load($OnLoadForm_StateCorrection)
@@ -71,4 +154,5 @@ $frmMain.ShowDialog()| Out-Null
 #------------------------------
 }
 GenerateForm
+
 
